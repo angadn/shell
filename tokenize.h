ba3cc0c1852d78139f42e7b1b4677d783bfdc760
Angad Nadkarni <angad@wootify.com>
2013-09-08 01:35:23 +0530

    Removed usage of redundant variable

--- tokenize.h
+++ tokenize.h
@@ -1,8 +1,7 @@
 char **tokenize(char *str, char *delims, char **tokens, int n) {
-    char *str_tmp = str;
     for (
         char **tok = tokens; 
-        (*tok = strsep(&str_tmp, delims)) != NULL && tok <= tokens + n;
+        (*tok = strsep(&str, delims)) != NULL && tok <= tokens + n;
         **tok == '\0' ? 0 : tok++
     );
 
