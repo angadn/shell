89b157abcca846f7075c7356b5d0619a59a30935
Angad Nadkarni <angad@wootify.com>
2013-09-08 01:03:09 +0530

    Moved in the variable declaration since we don't need it outside the loop

--- tokenize.h
+++ tokenize.h
@@ -1,8 +1,7 @@
 char **tokenize(char *str, char *delims, char **tokens, int n) {
     char *str_tmp = str;
-    char **tok;
     for (
-        tok = tokens; 
+        char **tok = tokens; 
         (*tok = strsep(&str_tmp, delims)) != NULL && tok <= tokens + n;
         tok++
     ) {
